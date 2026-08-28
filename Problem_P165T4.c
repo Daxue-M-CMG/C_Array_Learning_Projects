35d179cf9d1ad66136f6f777e4e3ec3794bd0a8d
RYE <daxue-m@qq.com>
2026-08-28 11:28:50 +0800

    feat: -可以自定义数组长度 style: 使用Google代码风格

--- Problem_P165T4.c
+++ Problem_P165T4.c
@@ -1,33 +1,35 @@
-//!有一个已经排好序的数组，要求输入一个数后，按照原来的排序规律将其插入
+//! 有一个已经排好序的数组，要求输入一个数后，按照原来的排序规律将其插入
 #include <stdio.h>
-int main(void)
-{
-    int a[14]={1,2,4,6,8,10,11,13,15,17,19,50,100};
-    int i,j,num,n=13;//*默认追加到末尾
-    printf("请输入一个数: \n");
-    scanf("%d",&num);
-    printf("原数组为: \n");//*输出原数组
-    for(i=0;i<13;i++)//*输入要插入的数
-    {
-        printf("a[%-2d]= %-4d\n",i+1,a[i]);
+int main(void) {
+  int a[101] = {0};
+  int i, j, n, num;
+  int pos;
+  printf("请输入数组长度: \n");
+  scanf("%d", &n);
+  pos = n;  //*默认插入到最后
+  printf("请按照 升序(从小到大) 输入数据。\n");
+  for (i = 0; i < n; i++) {
+    printf("a[%-2d]= ", i + 1);
+    scanf("%d", &a[i]);
+  }
+  printf("请输入要插入的数据: ");
+  scanf("%d", &num);
+  for (i = 0; i < n; i++)  //*寻找插入点
+  {
+    if (a[i] > num) {
+      pos = i;
+      break;
     }
-    for (i = 0; i < 13; i++) //!寻找插入位置
-    {
-        if (a[i] > num) 
-        {
-            n = i;
-            break;
-        }
-    }
-    for(j=12;j>=n;j--)//*数据挪动
-    {
-        a[j+1]=a[j];
-    }
-    a[n]=num;//*数据插入
-    printf("\n插入后数组为: \n");//*输出插入后的数组
-    for(i=0;i<14;i++)
-    {
-        printf("a[%-2d]= %-4d\n",i+1,a[i]);
-    }
-    return 0;
+  }
+  for (j = n - 1; j >= pos; j--)  //*挪动数组元素
+  {
+    a[j + 1] = a[j];
+  }
+  a[pos] = num;  //*插入数据
+  n++;
+  printf("现数组为: \n");
+  for (i = 0; i < n; i++) {
+    printf("a[%-2d]= %-4d\n", i + 1, a[i]);
+  }
+  return 0;
 }
